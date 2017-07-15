e0ab8122b2f9d92aec83b00fa0d6d2e573e0fdde
elipmoc <elipmoc@-PC>
2017-07-15 19:37:33 +0900

    SivSivSivSivSivSivSivSivSivSivSivSivSivSivSivSivSivSiv3D

---
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Main.cpp
@@ -0,0 +1,10 @@
+﻿#include "UseSiv3D.hpp"
+void Main()
+{
+	siv::Font font(20);
+	siv::String text;
+	while (siv::System::Update()){
+		siv::Input::GetCharsHelper(text);
+		font(text).draw();
+	}
+}
---
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/UseSiv3D.hpp
@@ -0,0 +1,4 @@
+#pragma once
+#define NO_S3D_USING
+#include <Siv3D.hpp>
+namespace siv = s3d;
