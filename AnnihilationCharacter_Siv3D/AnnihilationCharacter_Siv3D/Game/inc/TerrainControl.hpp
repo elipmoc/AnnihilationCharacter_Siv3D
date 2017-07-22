9bcfa38f7296bfc622451c7f7586c3b2beada45c
elipmoc <elipmoc@-PC>
2017-07-22 20:59:08 +0900

    フォルダ整理とwindowサイズ変更

--- AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/TerrainControl.hpp
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/TerrainControl.hpp
@@ -12,7 +12,7 @@ namespace game {
 		//特定のレーンの地形の高さを得る
 		double GetTerrainY(size_t lane)const {
 			if (lane >= LANE_NUM)throw siv::String(L"無効なレーン番号です");
-			return 400 - lane * 150;
+			return 500 - lane * 150;
 		}
 
 		//あるX座標のあるレーンに地形があるかどうか
@@ -23,16 +23,22 @@ namespace game {
 		TerrainControl();
 		void Update();
 	private:
+
 		//地形情報のリスト
 		std::deque<bool>m_terrainFlags[LANE_NUM];
+
 		//現在画面に表示されている地形
 		std::deque<bool> m_activeTerrains[LANE_NUM];
+
 		//表示用の地形文字
 		const siv::Font m_terrainFont;
+
 		//デバック用
 		siv::Circle circle;
+
 		//文字の高さ補正用（文字は表示位置がずれるため、ギャップを埋めなけらばならぬ）
 		static constexpr size_t FIX_TERRAIN_Y=19;
+
 		//地形の座標をずらす
 		double m_offset = 0;
 
--- AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Main.cpp
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Main.cpp
@@ -5,7 +5,7 @@
 void Main()
 {
 	siv::Window::SetTitle(L"消滅文字 ver 1919810114514");
-	siv::Window::Resize(700, 480);
+	siv::Window::Resize(700, 580);
 	game::MySceneManager mySceneManager;
 	mySceneManager.add<game::TitleScene>(L"Title");
 	mySceneManager.add<game::SelectPlayerScene>(L"SelectPlayer");
