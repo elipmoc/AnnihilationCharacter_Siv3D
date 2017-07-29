5bfd3f4d4ffd1aec2fa880a1f5b52bf6e15145ae
elipmoc <elipmoc@-PC>
2017-07-29 14:18:00 +0900

    ParticleBaseをelipmocframeworkに入れようかなあ、悩むなあ

--- AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/ParticleBase.hpp
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/ParticleBase.hpp
@@ -2,11 +2,17 @@
 namespace game {
 	class ParticleBase {
 		int m_lifeTime;
+		//”­‰Î‚ÌŠÔŠu
+		int m_fireInterval;
 	public:
 
-		ParticleBase(int lifeTime=0):m_lifeTime(lifeTime){}
+		ParticleBase(int fireInterval,int lifeTime):m_lifeTime(lifeTime),m_fireInterval(fireInterval){}
 
 		int GetLifeTime() const{ return m_lifeTime; }
+
+		void BaseInit() {
+			Init();
+		}
 
 		void BaseUpdate() {
 			Update();
@@ -16,8 +22,11 @@ namespace game {
 			Draw();
 		}
 	protected:
+		virtual void Init() = 0;
 		virtual void Draw()const=0;
 		virtual void Update()=0;
+		//”­‰Î
+		virtual void Fire() = 0;
 
 
 	};
--- AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/PlayerDamageParticle.hpp
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/PlayerDamageParticle.hpp
@@ -1,1 +1,11 @@
 #pragma once
+#include "ParticleBase.hpp"
+namespace game {
+	class PlayerDamageParticle:public ParticleBase {
+		virtual void Init()override;
+		virtual void Draw()const override;
+		virtual void Update()override;
+		virtual void Fire()override;
+
+	};
+}
