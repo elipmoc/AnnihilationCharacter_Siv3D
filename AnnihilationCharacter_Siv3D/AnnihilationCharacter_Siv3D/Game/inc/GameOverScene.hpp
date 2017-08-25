1b2a6c47f61d66fb06d43f6e4aaaa038659514ba
elipmoc <elipmoc@-PC>
2017-08-25 21:54:57 +0900

    ゲームおーばの文字を表示するようにした

--- AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/GameOverScene.hpp
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/GameOverScene.hpp
@@ -1,9 +1,13 @@
 #pragma once
 #include "SceneBase.hpp"
 
+
 namespace game {
 
+
 	class GameOverScene :public MySceneBase {
+		siv::Font font1;
+		siv::Font font2;
 	public:
 
 		virtual void init()override {
@@ -12,6 +16,9 @@ namespace game {
 		virtual void update()override {
 			if (siv::Input::KeyEnter.clicked)
 				changeScene(L"Title");
+			font1(L"‚ ‚È‚½‚Í‚µ‚É‚Ü‚µ‚½").drawCenter(siv::Window::Center());
+			font2(L"m9(^„D^)").drawCenter(siv::Window::Center().x,siv::Window::Center().y+50);
+			
 		}
 
 		virtual void draw() const override {
