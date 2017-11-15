4ad8a6ba0bc963c21beba290f8a52610cfaccfd4
elipmoc <elipmoc@-PC>
2017-11-15 23:51:35 +0900

    BossEnemyTalkの位置修正

--- AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/source/BossPhase.cpp
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/source/BossPhase.cpp
@@ -8,7 +8,7 @@
 
 namespace game {
 	BossPhase::BossPhase(Level level, const siv::Vec2 & playerRefPos, BulletListCreator & bulletListCreator,TerrainControl& terrainControl):
-		m_test(std::make_unique<BossEnemyTalk>(siv::Vec2(160,70))),
+		m_test(std::make_unique<BossEnemyTalk>(siv::Vec2(130,70))),
 		m_terrainControl(terrainControl),
 		m_boss(std::make_unique<Boss>(level,playerRefPos,bulletListCreator)),
 		m_powerSpawn(std::make_unique<PowerSpawn>())
