7b741db601449419685ccfdc489799ece49a2742
elipmoc <elipmoc@-PC>
2017-11-17 16:48:27 +0900

    BossHpGageクラスを作成

---
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/BossHpGage.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+namespace game {
+
+	class BossHpGage {
+		const size_t m_maxhp;
+	public:
+		BossHpGage(size_t maxhp):m_maxhp(maxhp) {
+			
+		}
+
+		void Draw(size_t hp)const {
+
+		}
+	};
+
+}
