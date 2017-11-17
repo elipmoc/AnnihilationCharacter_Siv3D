aea5913872c52e75c0585104d7d67870f87ec805
elipmoc <elipmoc@-PC>
2017-11-17 16:21:05 +0900

    powerGageのレイアウト変更

--- AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/PowerGage.hpp
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/PowerGage.hpp
@@ -5,19 +5,20 @@ namespace game {
 	//取得した力がどれくらいかを示すゲージのクラス
 	class PowerGage {
 		siv::Rect m_rect;
+		const siv::Font m_font;
 		const size_t m_maxPowerNum;
 	public:
-		PowerGage(size_t maxPowerNum):m_maxPowerNum ( maxPowerNum) {
-			
-			//まだ実装していない
-			m_rect.setPos(300, 20);
+		PowerGage(size_t maxPowerNum):
+			m_maxPowerNum ( maxPowerNum),m_font(20){
+			m_rect.setPos(410, 10);
 			
 		}
 		void Draw(size_t powerNum) {
-			//まだ実装していない
-			m_rect.setSize(200*powerNum /m_maxPowerNum, 20);
+			if (powerNum > m_maxPowerNum)
+				powerNum = m_maxPowerNum;
+			m_rect.setSize(250*powerNum /m_maxPowerNum, 20);
 			m_rect.draw(siv::Palette::Orange);
-			
+			m_font(L"パワー  :").draw({ 290, 0 }, siv::Palette::Orange);
 		}
 
 	};
