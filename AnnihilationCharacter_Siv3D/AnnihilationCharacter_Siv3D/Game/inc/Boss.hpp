36a737dcc481415342f5d8cd8061608b4aaf81c6
elipmoc <relipmoc101@gmail.com>
2017-11-18 13:47:32 +0900

    bossにhpをもたせた

--- AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/Boss.hpp
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/Boss.hpp
@@ -14,6 +14,7 @@ namespace game {
 	class Boss {
 		std::unique_ptr<BossBehavior> m_bossBehavior;
 		const Level m_level;
+		size_t m_hp;
 		std::unique_ptr<BulletListCreator> m_bulletListCreator;
 		const siv::Vec2& m_playerRefPos;
 		std::unique_ptr<elipmocframework::FontObject> m_fontObject;
