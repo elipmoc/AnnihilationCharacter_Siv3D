dfcfed2898e6a17a15f77ade40a0fdecca2e2509
elipmoc <relipmoc101@gmail.com>
2017-11-28 15:44:44 +0900

    BossBehaviorMakeを実装

--- AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/BossBehaviorMake.hpp
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/inc/BossBehaviorMake.hpp
@@ -1,1 +1,26 @@
 #pragma once
+#include <memory>
+
+namespace game {
+
+	class BulletListCreator;
+	class BossBehavior;
+
+	//任意のBossBehaviorを生成する基底のクラス
+	class BossBehaviorMakeBase {
+	public:
+		virtual std::unique_ptr<BossBehavior> Create(BulletListCreator*)=0;
+
+	};
+
+	//上記のクラスをテンプレートで実現
+	template<class T>
+	class BossBehaviorMake:BossBehaviorMakeBase {
+	public:
+		virtual std::unique_ptr<BossBehavior> Create(BulletListCreator* bulletListCreator) override final {
+			return std::make_unique<T>(bulletListCreator);
+		}
+
+	};
+
+}
