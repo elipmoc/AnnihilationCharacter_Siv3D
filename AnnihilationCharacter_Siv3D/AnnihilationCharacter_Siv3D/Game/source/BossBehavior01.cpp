20b925ca6caaa3d2d11328da6fd386e24430b876
elipmoc <relipmoc101@gmail.com>
2017-12-02 00:18:24 +0900

    BossBehavior01の実装した

--- AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/source/BossBehavior01.cpp
+++ AnnihilationCharacter_Siv3D/AnnihilationCharacter_Siv3D/Game/source/BossBehavior01.cpp
@@ -4,34 +4,160 @@
 #include "Boss.hpp"
 
 namespace game {
-	BossBehavior01::BossBehavior01(const Level level,const siv::Vec2& playerRefPos,const BulletListCreator& bulletListCreator):
-		BossBehavior(level,playerRefPos,bulletListCreator)
+	BossBehavior01::BossBehavior01(const Level level, const siv::Vec2& playerRefPos, const BulletListCreator& bulletListCreator) :
+		BossBehavior(level, playerRefPos, bulletListCreator)
 	{
 	}
 	void BossBehavior01::YawarakaUpdate()
 	{
-		if (count % 30 == 0) {
-			const auto addPos=(GetPlayerPos() - siv::Vec2{50,50})/ siv::Vec2{ 50,50 }.distanceFrom(GetPlayerPos());
-			GetBulletListCreator().MakeBullet(BulletInfo{ { 50 ,50 },addPos,{ 0,0 } });
+		switch (count2)
+		{
+
+		case 0:
+		{
+			BulletInfo bInfos[2];
+			bInfos[0] = BulletInfo{ Boss::BOSS_POS + siv::Vec2{ -10,-20 },{ -3,0 },{ 0,0 } };
+			bInfos[1] = BulletInfo{ Boss::BOSS_POS + siv::Vec2{ -10,100 },{ -3,0 },{ 0,0 } };
+			bInfos[0].glowFlag = true;
+			bInfos[1].glowFlag = true;
+			bInfos[0].glowColor = siv::Palette::Lightyellow;
+			bInfos[1].glowColor = siv::Palette::Lightyellow;
+			for (int j = 0; j < 2; j++) {
+				GetBulletListCreator().MakeBullet(bInfos[j]);
+				for (int i = 1; i < 2; i++) {
+					for (int e = -1; e < 2; e += 2) {
+						double angle = e*i * 20 / 180.0*siv::Math::Pi;
+						bInfos[j].speed = siv::Vec2{ -siv::Math::Cos(angle),siv::Math::Sin(angle) }*3;
+						GetBulletListCreator().MakeBullet(bInfos[j]);
+					}
+				}
+			}
+			count2++;
+		}
+		break;
+
+		case 1:
+			if (count > 120 && count % 30 == 0 && count < 120 + 30 * 5) {
+				auto bInfo = BulletInfo{ Boss::BOSS_POS + siv::Vec2{ -10,50 },{ -3,0 },{ 0,0 } };
+				bInfo.glowFlag = true;
+				bInfo.glowColor = siv::Palette::Lightyellow;
+				GetBulletListCreator().MakeBullet(bInfo);
+				for (int i = 1; i < 3; i++) {
+					for (int e = -1; e < 2; e += 2) {
+						double angle = e*i * 15 / 180.0*siv::Math::Pi;
+						bInfo.speed = siv::Vec2{ -siv::Math::Cos(angle),siv::Math::Sin(angle) }*3;
+						GetBulletListCreator().MakeBullet(bInfo);
+					}
+				}
+			}
+			else if (count >= 120 + 30 * 5 + 120) {
+				count2++;
+				count = 0;
+			}
+			break;
+
+		case 2:
+			int interval = 5;
+			int kaisu = 18;
+			if (count >= 50 && count % interval == 0 && count < 50 + interval * kaisu + 10) {
+				auto bInfo = BulletInfo{ Boss::BOSS_POS + siv::Vec2{ -20,0 },{ 0,0 },{ 0,0 } };
+				for (int e = -1; e < 2; e += 2) {
+					bInfo.color = siv::HSV::HSV(siv::Random(0, 255));
+					double angle = e*(count - 50) / (double)interval * 360 / kaisu / 180.0*siv::Math::Pi;
+					bInfo.speed = siv::Vec2{ -siv::Math::Cos(angle),siv::Math::Sin(angle) }*3;
+					GetBulletListCreator().MakeBullet(bInfo);
+				}
+			}
+			else if (count >= 50 + interval* kaisu + 10 + 120) {
+				count2 = 0;
+				count = 0;
+			}
+			break;
 		}
 		count++;
 	}
-	void BossBehavior01::NormalUpdate()
-	{
-		if (count % 20 == 0) {
-			GetBulletListCreator().MakeBullet(BulletInfo{ { 50 ,50 },{ 1,1 },{ 0,0 } });
+	void BossBehavior01::NormalUpdate() {
+		switch (count2)
+		{
+
+		case 0:
+		{
+			BulletInfo bInfos[2];
+			bInfos[0] = BulletInfo{ Boss::BOSS_POS + siv::Vec2{ -10,-20 },{ -3,0 },{ 0,0 } };
+			bInfos[1] = BulletInfo{ Boss::BOSS_POS + siv::Vec2{ -10,100 },{ -3,0 },{ 0,0 } };
+			bInfos[0].glowFlag = true;
+			bInfos[1].glowFlag = true;
+			bInfos[0].glowColor = siv::Palette::Lightyellow;
+			bInfos[1].glowColor = siv::Palette::Lightyellow;
+			for (int j = 0; j < 2; j++) {
+				GetBulletListCreator().MakeBullet(bInfos[j]);
+				for (int i = 1; i < 3; i++) {
+					for (int e = -1; e < 2; e += 2) {
+						double angle = e*i * 10 / 180.0*siv::Math::Pi;
+						bInfos[j].speed = siv::Vec2{ -siv::Math::Cos(angle),siv::Math::Sin(angle) }*3;
+						GetBulletListCreator().MakeBullet(bInfos[j]);
+					}
+				}
+			}
+			count2++;
+		}
+		break;
+
+		case 1:
+			if (count > 120 && count % 30 == 0 && count < 120 + 30 * 5) {
+				auto bInfo = BulletInfo{ Boss::BOSS_POS + siv::Vec2{ -10,50 },{ -3,0 },{ 0,0 } };
+				bInfo.glowFlag = true;
+				bInfo.glowColor = siv::Palette::Lightyellow;
+				GetBulletListCreator().MakeBullet(bInfo);
+				for (int i = 1; i < 4; i++) {
+					for (int e = -1; e < 2; e += 2) {
+						double angle = e*i * 12 / 180.0*siv::Math::Pi;
+						bInfo.speed = siv::Vec2{ -siv::Math::Cos(angle),siv::Math::Sin(angle) }*3;
+						GetBulletListCreator().MakeBullet(bInfo);
+					}
+				}
+			}
+			else if (count >= 120 + 30 * 5 + 120) {
+				count2++;
+				count = 0;
+			}
+			break;
+
+		case 2:
+			int interval = 5;
+			int kaisu = 36;
+			if (count >= 50 && count % interval == 0 && count < 50 + interval * kaisu + 10) {
+				auto bInfo = BulletInfo{ Boss::BOSS_POS + siv::Vec2{ -20,0 },{ 0,0 },{ 0,0 } };
+				for (int e = -1; e < 2; e += 2) {
+					bInfo.color = siv::HSV::HSV(siv::Random(0, 255));
+					double angle = e*(count - 50) / (double)interval * 360 / kaisu / 180.0*siv::Math::Pi;
+					bInfo.speed = siv::Vec2{ -siv::Math::Cos(angle),siv::Math::Sin(angle) }*3;
+					GetBulletListCreator().MakeBullet(bInfo);
+				}
+			}
+			else if (count >= 50 + interval* kaisu + 10 + 120) {
+				count2 = 0;
+				count = 0;
+			}
+			break;
 		}
 		count++;
 	}
+
 	void BossBehavior01::RengokuUpdate()
 	{
 		switch (count2)
 		{
+
 		case 0:
 		{
 			BulletInfo bInfos[2];
 			bInfos[0] = BulletInfo{ Boss::BOSS_POS + siv::Vec2{ -10,-20 },{ -3,0 },{ 0,0 } };
 			bInfos[1] = BulletInfo{ Boss::BOSS_POS + siv::Vec2{-10,100},{ -3,0 },{ 0,0 } };
+			bInfos[0].glowFlag = true;
+			bInfos[1].glowFlag = true;
+			bInfos[0].glowColor = siv::Palette::Lightyellow;
+			bInfos[1].glowColor = siv::Palette::Lightyellow;
 			for (int j = 0; j < 2; j++) {
 				GetBulletListCreator().MakeBullet(bInfos[j]);
 				for (int i = 1; i < 3; i++) {
@@ -45,9 +171,12 @@ namespace game {
 			count2++;
 		}
 			break;
+
 		case 1:
 			if (count>120 && count % 30 == 0 && count < 120+30 * 5) {
 				auto bInfo = BulletInfo{ Boss::BOSS_POS + siv::Vec2{ -10,50 },{ -3,0 },{ 0,0 } };
+				bInfo.glowFlag = true;
+				bInfo.glowColor = siv::Palette::Lightyellow;
 				GetBulletListCreator().MakeBullet(bInfo);
 				for (int i = 1; i < 5; i++) {
 					for (int e = -1; e < 2; e += 2) {
@@ -58,7 +187,25 @@ namespace game {
 				}
 			}
 			else if (count >= 120 + 30 * 5+120) {
-				count2 = 0;
+				count2 ++;
+				count = 0;
+			}
+			break;
+
+		case 2:
+			int interval=5;
+			int kaisu=72;
+			if (count >= 50 && count % interval == 0 && count < 50 + interval * kaisu + 10) {
+				auto bInfo = BulletInfo{ Boss::BOSS_POS + siv::Vec2{ -20,0 },{ 0,0 },{ 0,0 } };
+				for (int e = -1; e < 2; e += 2) {
+					bInfo.color = siv::HSV::HSV(siv::Random(0, 255));
+					double angle = e*(count - 50) / (double)interval * 360/kaisu / 180.0*siv::Math::Pi;
+					bInfo.speed = siv::Vec2{ -siv::Math::Cos(angle),siv::Math::Sin(angle) }*3;
+					GetBulletListCreator().MakeBullet(bInfo);
+				}
+			}
+			else if (count >= 50 + interval* kaisu + 10+120) {
+				count2=0;
 				count = 0;
 			}
 			break;
